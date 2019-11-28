f03fedabdb4a2ff157f8b02bfaf3433021731e85
HuangHeng <973157930@qq.com>
2019-11-28 14:44:40 +0800

    ds03_单向链表（获取结点个数、倒数第K个结点、反转链表）

--- src/linkedlist/SingleLinkedListDemo.java
+++ src/linkedlist/SingleLinkedListDemo.java
@@ -11,11 +11,8 @@ public class SingleLinkedListDemo {
         list.addByOrder(n2);
         list.addByOrder(n1);
         list.addByOrder(n3);
-
         list.list();
-        System.out.println("----------");
-        list.delete(2);
-        list.list();
+        System.out.println(list.findLastIndexNode(4));
     }
 }
 
@@ -111,6 +108,61 @@ class  SingleLinkedList {
         }
     }
 
+    //获取单链表的结点个数（不统计头结点）
+    public int getLength(Node head) {
+        if (head.next == null) return 0;
+        Node temp = head.next;
+        int length = 0;
+        while (temp != null) {
+            length++;
+            temp = temp.next;
+        }
+        return length;
+    }
+
+    //查找单链表中的倒数第k个结点
+    //双指针，
+    public Node findLastIndexNode(int index) {
+        if (index < 0) throw new RuntimeException("下标异常");
+        Node fast = head.next;
+        Node slow = head.next;
+        int count = 0;
+        while (fast != null) {
+            if (count < index) {
+                fast = fast.next;
+                count++;
+            } else if (count == index) {
+                fast = fast.next;
+                slow = slow.next;
+            }
+        }
+
+        if (count < index) {
+            throw new RuntimeException("超过链表长度");
+        } else {
+            return slow;
+        }
+    }
+
+    //单链表的反转
+    //创建一个新的链表，遍历原先链表，每遍历一个，就将其加入到新链表的开头
+    public void reverse() {
+        //链表为空，或只有一个结点，直接返回
+        if (head.next == null || head.next.next == null) {
+            return;
+        }
+        Node newHead = new Node(0, "");
+
+        Node temp = head.next;
+        Node next = null;
+        while (temp != null) {
+            next = temp.next;//保存当前结点的下一个结点
+            temp.next = newHead.next;
+            temp = next;
+        }
+        head.next = newHead.next;
+    }
+
     //打印链表
     public void list() {
         Node temp = head.next;
@@ -123,6 +175,10 @@ class  SingleLinkedList {
             temp = temp.next;
         }
 
+    }
+
+    public Node getHead() {
+        return head;
     }
 }
 
